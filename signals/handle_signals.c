076e4409de9ef387569a1380e0d2a3bb601c4575
El Mostafa El Bahtouri <elel-bah@e3r2p4.1337.ma>
2024-09-19 10:53:51 +0100

    fix the problem of the tester fail sometimes alhamdulillah

--- signals/handle_signals.c
+++ signals/handle_signals.c
@@ -3,101 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   handle_signals.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: messkely <messkely@student.42.fr>          +#+  +:+       +#+        */
+/*   By: elel-bah <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/29 17:03:53 by elel-bah          #+#    #+#             */
-/*   Updated: 2024/09/07 12:01:12 by messkely         ###   ########.fr       */
+/*   Created: 2024/09/19 10:52:04 by elel-bah          #+#    #+#             */
+/*   Updated: 2024/09/19 10:52:06 by elel-bah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mini_shell.h"
 
-void sig_int(int code)
+void	sig_int(int code)
 {
-    (void)code;
-    if (get_in_heredoc(-500))
-    {
-        write(STDERR_FILENO, "\n", 1);
-        get_sigint(1);
-        get_exit_status(1);
-    }
-    else if (get_pid(-500) == 0)
-    {
-        write(STDERR_FILENO, "\n", 1);
-        rl_on_new_line();
-        rl_replace_line("", 0);
-        rl_redisplay();
-        get_sigint(1);
-        get_exit_status(1);
-    }
-    else
-    {
-        write(STDERR_FILENO, "\n", 1);
-    }
+	(void)code;
+	if (get_in_heredoc(-500))
+	{
+		write(STDERR_FILENO, "\n", 1);
+		get_sigint(1);
+		get_exit_status(1);
+	}
+	else if (get_pid(-500) == 0)
+	{
+		write(STDERR_FILENO, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+		get_sigint(1);
+		get_exit_status(1);
+	}
+	else
+	{
+		write(STDERR_FILENO, "\n", 1);
+	}
 }
 
-void sig_quit(int code)
+void	sig_quit(int code)
 {
-    char *nbr;
-    nbr = ft_itoa(code);
-    if (get_pid(-500) != 0)
-    {
-        ft_putstr_fd("Quit: \n", STDERR_FILENO);
-        ft_putendl_fd(nbr, STDERR_FILENO);
-       	get_exit_status(131);
-        get_sigquit(1);
-    }
-    // else
-    //     ft_putstr_fd("\b\b  \b\b", STDERR_FILENO);
-    free(nbr);
+	char	*nbr;
+
+	nbr = ft_itoa(code);
+	if (get_pid(-500) != 0)
+	{
+		ft_putstr_fd("Quit: \n", STDERR_FILENO);
+		ft_putendl_fd(nbr, STDERR_FILENO);
+		get_exit_status(131);
+		get_sigquit(1);
+	}
+	free(nbr);
 }
 
-void sig_init(void)
+void	sig_init(void)
 {
-    get_sigint(0);
-    get_sigquit(0);
-    get_pid(0);
-    get_exit_status(0);
+	get_sigint(0);
+	get_sigquit(0);
+	get_pid(0);
+	get_exit_status(0);
 }
-
-// int get_sigint(int n)
-// {
-// 	static int v = 0;
-	
-// 	if (n != -500)
-// 		v = n;
-// 	return (v);
-// }
-// int get_sigquit(int n)
-// {
-// 	static int v = 0;
-	
-// 	if (n != -500)
-// 		v = n;
-// 	return (v);
-// }
-// int get_pid(int n)
-// {
-// 	static int v = 0;
-	
-// 	if (n != -500)
-// 		v = n;
-// 	return (v);
-// }
-// int get_exit_status(int n)
-// {
-// 	static int v = 0;
-	
-// 	if (n != -500)
-// 		v = n;
-// 	return (v);
-// }
-// int get_in_heredoc(int n)
-// {
-// 	static int v = 0;
-	
-// 	if (n != -500)
-// 		v = n;
-// 	return (v);
-// }
-
--- signals/sig_sig.c
+++ signals/sig_sig.c
@@ -1,41 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sig_sig.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elel-bah <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 10:51:37 by elel-bah          #+#    #+#             */
+/*   Updated: 2024/09/19 10:51:38 by elel-bah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../mini_shell.h"
 
-int get_sigint(int n)
+int	get_sigint(int n)
 {
-	static int v = 0;
-	
+	static int	v;
+
 	if (n != -500)
 		v = n;
 	return (v);
 }
-int get_sigquit(int n)
+
+int	get_sigquit(int n)
 {
-	static int v = 0;
-	
+	static int	v;
+
 	if (n != -500)
 		v = n;
 	return (v);
 }
-int get_pid(int n)
+
+int	get_pid(int n)
 {
-	static int v = 0;
-	
+	static int	v;
+
 	if (n != -500)
 		v = n;
 	return (v);
 }
-int get_exit_status(int n)
+
+int	get_exit_status(int n)
 {
-	static int v = 0;
-	
+	static int	v;
+
 	if (n != -500)
 		v = n;
 	return (v);
 }
-int get_in_heredoc(int n)
+
+int	get_in_heredoc(int n)
 {
-	static int v = 0;
-	
+	static int	v;
+
 	if (n != -500)
 		v = n;
 	return (v);
